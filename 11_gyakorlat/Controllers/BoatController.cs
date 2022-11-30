9b10a244d150b9c0ea49022569d248f32ab2a444
hvdef7 <hvdef7@SZA0030898>
2022-11-30 10:31:18 +0100

    Add project files.

---
+++ 11_gyakorlat/Controllers/BoatController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace _11_gyakorlat.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public ActionResult M1()
+        {
+            Models.HajosContext context = new Models.HajosContext();
+            var kérdések = from x in context.Questions select x;
+                           return Ok(kérdések);
+
+            //return new JsonResult(kérdések);
+        }
+    }
+}
---
+++ 11_gyakorlat/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _11_gyakorlat.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+            return(Ok(pontosIdő));
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
+    }
+}
